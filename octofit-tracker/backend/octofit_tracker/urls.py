02fca65d9656860790afa308f38eabcdd06393c2
Luis Abrantes <166118654+lmabrantes@users.noreply.github.com>
2026-02-25 11:10:28 +0000

    Add redirect from root to API endpoint in URL configuration

--- octofit-tracker/backend/octofit_tracker/urls.py
+++ octofit-tracker/backend/octofit_tracker/urls.py
@@ -1,6 +1,7 @@
 import os
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 from rest_framework.routers import DefaultRouter
 from .views import (
     api_root, UserViewSet, TeamViewSet, ActivityViewSet,
@@ -21,6 +22,7 @@ else:
     base_url = "http://localhost:8000"
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/api/', permanent=False)),
     path('admin/', admin.site.urls),
     path('api/', api_root, name='api-root'),
     path('api/', include(router.urls)),
